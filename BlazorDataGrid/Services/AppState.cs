5bb9a8397d389f9e3addc50be2317e8d0828c969
Sébastien <niou128@gmail.com>
2019-08-03 11:20:09 +0200

    fisrt commit

---
+++ BlazorDataGrid/Services/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorDataGrid.Services
+{
+    public class AppState
+    {
+        public event Action RefreshRequested;
+        public void CallRequestRefresh()
+        {
+            RefreshRequested?.Invoke();
+        }
+    }
+}
