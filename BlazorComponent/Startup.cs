0ca98282380ba7b8a9191ced88b31237b31d5f99
Sébastien <niou128@gmail.com>
2019-08-03 17:34:52 +0200

    removed unused classes

--- BlazorComponent/Startup.cs
+++ BlazorComponent/Startup.cs
@@ -1,16 +1,10 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using BlazorComponent.Data;
+using BlazorDataGrid.Services;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using BlazorComponent.Data;
-using BlazorDataGrid.Services;
 
 namespace BlazorComponent
 {
