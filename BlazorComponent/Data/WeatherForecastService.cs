ffc6383d9f73992146915870ea0fe237bd43a0a8
Sébastien Bernard <niou128@hotmail.com>
2019-08-05 11:05:32 +0200

    resize the pagersize when filtering

--- BlazorComponent/Data/WeatherForecastService.cs
+++ BlazorComponent/Data/WeatherForecastService.cs
@@ -14,7 +14,7 @@ namespace BlazorComponent.Data
         public Task<WeatherForecast[]> GetForecastAsync(DateTime startDate)
         {
             var rng = new Random();
-            return Task.FromResult(Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            return Task.FromResult(Enumerable.Range(1, 20).Select(index => new WeatherForecast
             {
                 Date = startDate.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
