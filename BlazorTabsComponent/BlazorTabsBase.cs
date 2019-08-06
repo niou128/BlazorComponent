bdb007ce2f05ca641aeb981c2e9b66da01b6f66a
Sébastien Bernard <niou128@hotmail.com>
2019-08-06 16:09:13 +0200

    Add New component BlazorTabs

---
+++ BlazorTabsComponent/BlazorTabsBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+
+namespace BlazorTabsComponent
+{
+    public class BlazorTabsBase : ComponentBase
+    {
+        public string BlazorRocksText { get; set; } =
+            "Blazor rocks the browser!";
+    }
+}
