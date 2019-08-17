549a6552aff167d9195263c82bc2dcc9924d2e6e
Sébastien Bernard <niou128@hotmail.com>
2019-08-17 10:52:18 +0200

    update to preview 8

---
+++ BlazorComponent/Services/AppState.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace BlazorComponent.Services
+{
+    public class AppState
+    {
+        public event Action RefreshRequested;
+        public void CallRequestRefresh()
+        {
+            RefreshRequested?.Invoke();
+        }
+    }
+}
