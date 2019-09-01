7d9f12f33c08f81e6bc39665fa47ec2f080b34d6
Sébastien <niou128@gmail.com>
2019-09-01 14:00:34 +0200

    add item per page selector

---
+++ BlazorDataGrid/BlazorDataGridBase.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorDataGrid
+{
+    public class BlazorDataGridBase : ComponentBase
+    {       
+        [Parameter]
+        public bool ShowPageSelector { get; set; } = true;
+
+        protected List<PageSizeStruct> PageSizeList { get; set; }
+
+        public class PageSizeStruct
+        {
+            public string Label { get; set; }
+            public int Value { get; set; }
+        }
+
+        public enum Direction
+        {
+            First,
+            Previous,
+            Next,
+            PreviousSegment,
+            NextSegment,
+            Last
+        }
+    }
+}
