8a697ee164ee779381f692bc9b99dc7cc70b707e
Sébastien <niou128@gmail.com>
2019-09-02 21:25:46 +0200

    Add Debounce on filter

---
+++ BlazorDataGrid/BlazorGridColumnBase.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorDataGrid
+{
+    public class BlazorGridColumnBase : ComponentBase
+    {
+        Timer DebounceTimerInterval { get; set; }
+        Action<object> DebounceAction { get; set; }
+        object LastObjectDebounced { get; set; }
+
+        protected void Debounce(object obj, int interval, Action<object> debounceAction)
+        {
+            LastObjectDebounced = obj;
+            DebounceAction = debounceAction;
+
+            DebounceTimerInterval?.Dispose();
+
+            DebounceTimerInterval = new Timer(DebounceTimerIntervalOnTick, obj, interval, interval);
+        }
+
+        protected void DebounceTimerIntervalOnTick(object state)
+        {
+            DebounceTimerInterval?.Dispose();
+
+            if (DebounceTimerInterval != null)
+            {
+                DebounceAction?.Invoke(LastObjectDebounced);
+            }
+
+            DebounceTimerInterval = null;
+        }
+    }
+}
